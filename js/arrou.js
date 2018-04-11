d9f4647a4e90d78a861411c461e0930ed5f45b54
IvanZaharenko <34645208+IvanZaharenko@users.noreply.github.com>
2018-04-11 15:15:56 +0300

    Add files via upload

---
+++ js/arrou.js
@@ -0,0 +1,23 @@
+﻿<script>
+	jQuery(function($) {
+		var $toup = $('.up');
+		var body = $('html, body');
+		
+		$(window).scroll(function(){
+		
+		
+			if($(window).scrollTop()>1000){
+				$toup.fadeIn(300);
+			}
+			else if ($(window).scrollTop()<1000){
+				$toup.hide();
+			}
+		});
+		
+		$toup.on('click', function(e) {
+			e.preventDefault();
+			body.animate({scrollTop:0}, 600, 'swing');
+		});
+	});
+</script>
+
